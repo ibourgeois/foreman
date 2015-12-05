cb4ccffa45168012a20e3da50898978c6f993686
Derek Bourgeois <derek@ibourgeois.com>
2015-12-04 22:03:56 -0500

    Add new commands

---
+++ src/BlueprintCommand.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace iBourgeois\Foreman\Console;
+
+use RuntimeException;
+use GuzzleHttp\Client;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class BlueprintCommand extends Command
+{
+	
+}
